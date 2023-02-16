125837d25f41294578ebef948ca958c399296fbe
frontend2006 <aziretsarapov@gmail.com>
2023-02-16 18:54:03 +0600

    create sidebar and layout

--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,12 @@ import Container from "./container/Container";
 import Image from "./image/Image";
 import { Forms } from "./forms";
 import Loader from "./loader/Loader";
+import Sidebar from "./sideBar/Sidebar";
 
 export const Components = {
   Container,
   Image,
   Forms,
-  Loader
+  Loader,
+  Sidebar
 }
